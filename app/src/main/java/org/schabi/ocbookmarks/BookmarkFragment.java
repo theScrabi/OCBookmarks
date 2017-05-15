35eb9774e2ba9f43414c9cd7d28470d5d17fdc41
Christian Schabesberger <chris.schabesberger@mailbox.org>
2017-05-15 15:58:43 +0200

    put fab into bookmarks fragment

--- app/src/main/java/org/schabi/ocbookmarks/BookmarkFragment.java
+++ app/src/main/java/org/schabi/ocbookmarks/BookmarkFragment.java
@@ -1,6 +1,9 @@
 package org.schabi.ocbookmarks;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,16 @@ public class BookmarkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fagment_bookmarks, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         return rootView;
     }
 }
