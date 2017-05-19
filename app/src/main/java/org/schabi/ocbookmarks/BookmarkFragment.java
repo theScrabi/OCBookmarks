902a165b8b278c9f529ede075355ccfaf43e755f
Christian Schabesberger <chris.schabesberger@mailbox.org>
2017-05-19 22:02:51 +0200

    fix login page

--- app/src/main/java/org/schabi/ocbookmarks/BookmarkFragment.java
+++ app/src/main/java/org/schabi/ocbookmarks/BookmarkFragment.java
@@ -1,14 +1,10 @@
 package org.schabi.ocbookmarks;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by the-scrabi on 15.05.17.
@@ -19,15 +15,6 @@ public class BookmarkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fagment_bookmarks, container, false);
-
-        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         return rootView;
     }
